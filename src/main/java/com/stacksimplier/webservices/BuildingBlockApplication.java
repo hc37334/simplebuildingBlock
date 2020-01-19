0a7d0b3bb56e9dae99b8a915aeb4b8d6e99e7e2a
hsiaolinchu <33436866+hsiaolinchu@users.noreply.github.com>
2020-01-18 19:05:58 -0800

    first commit

---
+++ src/main/java/com/stacksimplier/webservices/BuildingBlockApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplier.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplier/webservices/BuildingBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplier.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
